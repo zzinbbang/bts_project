7b5eafeabc4a90f384c8f38308a72f727ed6dc13
zzinbbang <anaryeoni@gmail.com>
2023-03-08 19:07:36 +0900

    230308

--- btsweb/urls.py
+++ btsweb/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('main/', views.main, name='main'),
+    path('', views.main, name='main'),
     path('event/', views.event, name='event'),
     # path('check/', views.check, name='check')
     path('confirm/', views.confirm, name='confirm'),
--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('btsweb/', include('btsweb.urls'))
+    path('', include('btsweb.urls'))
 ]
